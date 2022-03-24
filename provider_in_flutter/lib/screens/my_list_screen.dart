1b483baeacc13cd06b16c047e39c3c27fe3c3fd4
wilker123 <josewilkerlucena123@gmail.com>
2022-03-24 18:17:52 -0300

    Finished MyListScreen

--- provider_in_flutter/lib/screens/my_list_screen.dart
+++ provider_in_flutter/lib/screens/my_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/movie_provider.dart';
 
 class MyListScreen extends StatefulWidget {
   const MyListScreen({Key? key}) : super(key: key);
@@ -10,6 +13,34 @@ class MyListScreen extends StatefulWidget {
 class _MyListScreenState extends State<MyListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _myList = context.watch<MovieProvider>().myList;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minha lista (${_myList.length})"),
+      ),
+      body: ListView.builder(
+        itemCount: _myList.length,
+        itemBuilder: (_, index) {
+          final currentMovie = _myList[index];
+          return Card(
+            key: ValueKey(currentMovie.title),
+            elevation: 4,
+            child: ListTile(
+              title: Text(currentMovie.title),
+              subtitle: Text(currentMovie.runtime ?? ''),
+              trailing: TextButton(
+                child: const Text(
+                  'Remove',
+                  style: TextStyle(color: Colors.red),
+                ),
+                onPressed: () {
+                  context.read<MovieProvider>().removeFromList(currentMovie);
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
