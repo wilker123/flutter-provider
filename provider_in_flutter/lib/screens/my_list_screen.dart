39dcaa4c8c2660c3da6ffe847e2b6fbfdcacccde
wilker123 <josewilkerlucena123@gmail.com>
2022-03-24 17:17:41 -0300

    Initializing implementation MyListScreen

---
+++ provider_in_flutter/lib/screens/my_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyListScreen extends StatefulWidget {
+  const MyListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyListScreen> createState() => _MyListScreenState();
+}
+
+class _MyListScreenState extends State<MyListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
