b04fde8415f9299605e0e9419e47254085f40fc9
wilker123 <josewilkerlucena123@gmail.com>
2022-03-24 18:22:42 -0300

    Implementing main.dart. Configurate provider

--- provider_in_flutter/lib/main.dart
+++ provider_in_flutter/lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'provider/movie_provider.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider<MovieProvider>(
+    child: const MyApp(),
+    create: (_) => MovieProvider(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Provider Project',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
