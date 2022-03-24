13bb9bf903cb666457c35a913304bf2a25b7778b
wilker123 <josewilkerlucena123@gmail.com>
2022-03-24 16:52:28 -0300

    Created listMovies in movie_provider! Created method addToList

--- provider_in_flutter/lib/provider/movie_provider.dart
+++ provider_in_flutter/lib/provider/movie_provider.dart
@@ -1,1 +1,26 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import '../models/movie.dart';
 
+final List<Movie> initialData = List.generate(
+  50,
+  (index) => Movie(
+    title: "Moview $index",
+    runtime: "${Random().nextInt(100) + 60} minutes",
+  ),
+);
+
+class MovieProvider with ChangeNotifier {
+  final List<Movie> _movies = initialData;
+
+  List<Movie> get movies => _movies;
+
+  final List<Movie> _myList = [];
+
+  List<Movie> get myList => _myList;
+
+  void addToList(Movie movie) {
+    _myList.add(movie);
+    notifyListeners();
+  }
+}
